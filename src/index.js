8f74c275cdbba5d0d54852b6f47ced490e9d4698
Sebastian MolinaS <sebastianmolina@Sebastians-MacBook-Pro.local>
2022-10-24 19:24:48 -0300

    adding routes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Contact from './components/Contact';
+import Nosotros from './components/Nosotros';
+import Tarifas from './components/Tarifas';
+import Servicios from './components/Servicios';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/nosotros" element={<Nosotros />} />
+      <Route path="/tarifas" element={<Tarifas />} />
+      <Route path="/servicios" element={<Servicios />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
